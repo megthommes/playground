e1f1a5ef1bb72e2753513e33716e8430d19dc9d6
Meghan Thommes <meghan.thommes@gmail.com>
2024-07-29 22:36:22 -0400

    create superuser

--- tutorials/django-vscode/web_project/urls.py
+++ tutorials/django-vscode/web_project/urls.py
@@ -20,5 +20,8 @@ from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
-urlpatterns = [path("", include("hello.urls")), path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("", include("hello.urls")),
+    path("admin/", admin.site.urls),
+]
 urlpatterns += staticfiles_urlpatterns()
