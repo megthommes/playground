275690bfda355fe540f4b65ad1476a1042c51af1
Meghan Thommes <meghan.thommes@gmail.com>
2024-07-29 21:57:14 -0400

    data models and migrations

--- tutorials/django-vscode/hello/views.py
+++ tutorials/django-vscode/hello/views.py
@@ -2,10 +2,20 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import ListView
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
 
 
-def home(request):
-    return render(request, "hello/home.html")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
 
 
 def about(request):
@@ -21,3 +31,16 @@ def hello_there(request, name):
     return render(
         request, "hello/hello_there.html", {"name": name, "date": datetime.now()}
     )
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
